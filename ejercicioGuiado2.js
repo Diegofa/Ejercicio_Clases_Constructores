40395243cf9d6245eb6d83477cc55686c8812582
diegofa <diegoangulo1027@gmail.com>
2023-01-30 22:57:05 -0500

    Primer abstraccion

---
+++ ejercicioGuiado2.js
@@ -0,0 +1,28 @@
+/*Abtracción
+
+-Tipos de transporte (Clase Principal o Padre)
+medio de desplazamiento --> Terrestre, Maritimo, Aereo
+Nombre; 
+Velocidad -- > Kilometros Hora; 
+Marca
+Capacidad de cupo
+
+- Transportes Motorizados
+Cantidad de ruedas 
+Consumo de gasolina
+Tipo de Motor
+
+
+- Transportes Electricos 
+Carga de la bateria 
+Duración de la batería
+
+
+-Transportes No motorizados
+
+Tipo de tracción --> Humana, Animal
+
+Bicicletas, triciclos
+Carretas, carrozas
+
+*/
